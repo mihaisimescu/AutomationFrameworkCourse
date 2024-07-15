e4f02d300dfafb57dd7a88fa01da0db66471e686
MihaiSimescu <mihai.simescu@emeal.nttdata.com>
2024-07-15 11:55:12 +0300

    Implemented cucumber test

--- src/test/java/stepDefinition/CalendarSteps.java
+++ src/test/java/stepDefinition/CalendarSteps.java
@@ -1,17 +1,22 @@
 package stepDefinition;
 
+import actions.Dashboard;
+import actions.Login;
+import actions.Register;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 import utils.BaseTest;
+import utils.ConfigLoader;
 
 public class CalendarSteps extends BaseTest {
 
     @Before
     public void beforeScenario(){
-        setupClass();;
+        setupClass();
         initTest("Cucumber Calendar Test");
     }
 
@@ -20,24 +25,54 @@ public class CalendarSteps extends BaseTest {
         tearDown();
     }
 
+    private Login login = null;
+    private Dashboard dashboard = null;
+    private Register register;
+
+
     @Given("User is logged in with email {string}, password {string}, and phone number {string}")
     public void user_is_logged_in_with_email_password_and_phone_number(String email, String password, String phoneNumber) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        login  = new Login(driver);
+        dashboard = new Dashboard(driver);
+        register = new Register(driver);
+
+        login(email, password);
+
     }
     @When("User navigates to the specific day {string}")
-    public void user_navigates_to_the_specific_day(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_navigates_to_the_specific_day(String date) {
+        dashboard.clickSpecificDay(date);
+
     }
     @When("User creates a new event {string}")
-    public void user_creates_a_new_event(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_creates_a_new_event(String eventText) {
+        dashboard.sendEventText(eventText);
+        dashboard.clickCreateEventButton();
+
     }
     @Then("The event {string} is present in the calendar")
-    public void the_event_is_present_in_the_calendar(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_event_is_present_in_the_calendar(String eventText) {
+
+        Assert.assertTrue(dashboard.isEventPresent(eventText));
+    }
+
+    private void login(String email, String password) {
+        loginActions(email, password);
+
+        if(login.errorForbiddenAccessText().equals("Access forbidden!")){
+            login.clickRegisterButton();
+            register.registerUser(true);
+            loginActions(email, password);
+        }
+
+        Assert.assertTrue(dashboard.getUserEmailFromDashBoard().equalsIgnoreCase(email));
+
+    }
+
+    private void loginActions(String email, String password) {
+        login.enterUserName(email);
+        login.enterPassword(password);
+        login.clickSubmit();
     }
 }
